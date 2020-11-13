e64ee39d51dfd2ec34ae61d8f98c8da57bfdf6ff
Uzi <uzifr@post.bgu.ac.il>
2020-11-13 17:40:55 +0200

    some cleanup on astar_agent.py

--- astar_agent.py
+++ astar_agent.py
@@ -4,7 +4,6 @@ from agent import Agent
 
 MAX = 10000
 
-
 def is_goal(graph, location):
     return graph.nodes(data=True)[location][1]['value'] > 0
 
@@ -19,34 +18,38 @@ def reconstruct_path(goal, predecessors):
     return ret
 
 
-def get_path(graph, location):
+def get_path(graph, location, heuristic):
     g_values = {edge: MAX for edge in graph.nodes()}
     g_values[location] = 0
-    f_values = {edge: MAX for edge in graph.nodes()}
-    f_values[location] = 0
+    f_values = g_values.copy()
     predecessors = {edge: None for edge in graph.nodes()}
-    found_goal = False
-    h = graph[location]
-    h = list(h.items())
-    h = [(f, s['weight']) for f, s in h]
+    h = list(f_values.items())
     _heapq.heapify(h)
-    while not found_goal:
+    while True:
         smallest = _heapq.heappop(h)
         if is_goal(graph, smallest):
             return reconstruct_path(smallest, predecessors)
         else:
             expansion = graph[smallest]
             expansion = list(expansion.items())
-            expansion = [(f, s['weight']) for f, s in expansion]
-            for item in expansion:
-                _heapq.heappush(h, item)
+            for neighbor in expansion:
+                new_possible_g = g_values[smallest] + neighbor[1]['weight']
+                if new_possible_g < g_values[neighbor[0]]:
+                    neighbor_id = neighbor[0]
+                    g_values[neighbor_id] = new_possible_g
+                    predecessors[neighbor_id] = smallest[0]
+                    f_values[neighbor_id] = new_possible_g + heuristic[neighbor]
+                    # TODO might not work as intended
+                    if neighbor_id not in h:
+                        _heapq.heappush(h, (neighbor[0], f_values[neighbor[0]]))
 
 
 class AStarAgent(Agent):
 
-    def __init__(self, aid):
+    def __init__(self, aid, heuristic=None):
         super().__init__(aid)
         self.curr_path = []
+        self.heuristic = heuristic
 
     def act(self, observation):
         if self.terminated:  # If terminated, return no-op
@@ -56,7 +59,8 @@ class AStarAgent(Agent):
         location = observation.locations[self.aid][1]
         if not self.curr_path:
             graph = observation.graph
-            self.curr_path = get_path(graph, location)
+            # TODO probably need to remove first vetex in path, easy fix but check
+            self.curr_path = get_path(graph, location, self.heuristic)
         next_vertex = self.curr_path[0]
         weight = observation.graph[location][next_vertex]['weight']
         action = ("move", location, next_vertex, weight)
