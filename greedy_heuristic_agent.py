72bc993a2cc7a86f90ceec79dfbf8a85ba83e4e4
Uzi <uzifr@post.bgu.ac.il>
2020-11-14 15:50:19 +0200

    some cleanup on astar_agent.py

---
+++ greedy_heuristic_agent.py
@@ -0,0 +1,36 @@
+from agent import Agent
+from networkx.algorithms.shortest_paths.weighted import single_source_dijkstra
+
+
+class GreedyHeuristicAgent(Agent):
+
+    def __init__(self, aid):
+        super().__init__(aid)
+
+    def act(self, observation):
+        if self.terminated:  # If terminated, return no-op
+            return ("noop",)
+        if self.is_moving(observation):  # if traversing on edge, return no-op
+            return ("noop",)
+        location = observation.locations[self.aid][1]
+        next_vertex = self.get_greedy_move(observation.graph, location)
+        weight = observation.graph[location][next_vertex]['weight']
+        action = ("move", location, next_vertex, weight)
+        return action
+
+    def get_greedy_move(self, graph, location):
+        next_vertex = None
+        cheapest = None
+        for neighbor in graph[location]:
+            if graph.nodes(data=True)['value'] > 0 and cheapest is None or neighbor[1]['weight'] < cheapest:
+                next_vertex = neighbor[0]
+                cheapest = neighbor[1]['weight']
+        if next_vertex is None:
+            for neighbor in graph[location]:
+                if cheapest is None or neighbor[1]['weight'] < cheapest:
+                    next_vertex = neighbor[0]
+                    cheapest = neighbor[1]['weight']
+        return next_vertex
+
+
+
